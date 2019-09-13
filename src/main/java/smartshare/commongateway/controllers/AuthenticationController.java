547e5328279ef71aacb8ab41eef8f01d7f482b9e
Sethuram Ramalinga Reddy <ramalins@tcd.ie>
2019-09-13 07:42:19 +0100

    forwareded signup request

--- src/main/java/smartshare/commongateway/controllers/AuthenticationController.java
+++ src/main/java/smartshare/commongateway/controllers/AuthenticationController.java
@@ -1,7 +1,6 @@
 package smartshare.commongateway.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -23,10 +22,32 @@ public class AuthenticationController {
         return "Hello !";
     }
 
-    @GetMapping(value="**")
-    public ResponseEntity<Map<String, String>> testMethod(RequestEntity request){
+//    @GetMapping(value="**")
+//    public ResponseEntity<Map<String, String>> testMethod(RequestEntity request){
+//        System.out.println("inside");
+//        System.out.println(request);
+//
+////        Have to sort out on HTTP status issue for correct and error result
+//
+//        return new ResponseEntity<>( service.processRequests(request),HttpStatus.OK);
+//    }
+
+    @RequestMapping(
+            value = "**",
+            method = {RequestMethod.GET, RequestMethod.POST}
+    )
+    public ResponseEntity<Map<String, String>> testPostMethod(RequestEntity request) {
         System.out.println("inside");
         System.out.println(request);
+
+//        Have to sort out on HTTP status issue for correct and error result
+
         return new ResponseEntity<>( service.processRequests(request),HttpStatus.OK);
     }
+
+//    @PostMapping(path = "/signUp", consumes = "application/json", produces = "application/json")
+//    public void addMember(@RequestBody RequestEntity request ) {
+//        System.out.println("inside");
+//        System.out.println(request);
+//    }
 }
--- src/main/java/smartshare/commongateway/services/AuthenticationService.java
+++ src/main/java/smartshare/commongateway/services/AuthenticationService.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-
 import java.util.Map;
 
 @Service
@@ -18,17 +17,30 @@ public class AuthenticationService {
     public Map<String, String> processRequests(RequestEntity request){
 
         System.out.println("inside service");
+        System.out.println(request.getUrl().getPath());
         String url = "";
+        ResponseEntity<Object> responseEntity = null;
 
         switch ( request.getUrl().getPath()){
 
+            case "/signUp":
+                System.out.println("inside signup");
+                url = "http://localhost:8082/signUp";
+                responseEntity = restTemplate.postForEntity(url, request.getBody(), Object.class);
+                break;
+ 
             case "/signIn":
+                System.out.println("inside signin");
                 url = "http://localhost:8082/signIn";
+                responseEntity = restTemplate.getForEntity(url, Object.class);
+                break;
+
+            default:
+                url = "http://localhost:8082/default";
 
         }
-        ResponseEntity<Object> responseEntity = restTemplate.getForEntity(url,Object.class);
+
         System.out.println("responseEntity");
-        System.out.println(responseEntity);
         return  (Map<String, String>) responseEntity.getBody();
     }
 }
